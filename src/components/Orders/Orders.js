b555a36f6f82a21895140b62852b7905db823197
ShahbajKhan <shktoky@gmail.com>
2022-10-02 14:05:33 +0600

    this commit contains the content of video eight

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import useCart from '../../hooks/useCart';
-import useProducts from '../../hooks/useProducts';
-import Cart from '../Cart/Cart';
-import ReviewItem from '../ReviewItem/ReviewItem';
+import React from "react";
+import { Link } from "react-router-dom";
+import useCart from "../../hooks/useCart";
+import useProducts from "../../hooks/useProducts";
+import { removeFromDb } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
-    const [products, setProducts] = useProducts()
-    const [cart, setCart] = useCart(products)
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useCart(products);
 
-    const handleRemoveProduct = product =>{
-        const rest = cart.filter(pd => pd.id !== product.id);
-        setCart(rest);
-    }
+  const handleRemoveProduct = (product) => {
+    const rest = cart.filter((pd) => pd.id !== product.id);
+    setCart(rest);
+    removeFromDb(product.id);
+  };
 
-    return (
-        <div className='shop-container'>
-            <div className="products-container">
-                {
-                    cart.map(product => <ReviewItem
-                        key={product.id}
-                        product={product}
-                        handleRemoveProduct={handleRemoveProduct}
-                    ></ReviewItem>)
-                }
-            </div>
-            <div className="cart-container">
-                <Cart cart={cart}></Cart>
-            </div>
-        </div>
-    );
+  return (
+    <div className="shop-container">
+      <div className="review-items-container">
+        {cart.map((product) => (
+          <ReviewItem
+            key={product.id}
+            product={product}
+            handleRemoveProduct={handleRemoveProduct}
+          ></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}>
+          <Link to="/inventory">
+            <button>Proceed Checkout </button>
+          </Link>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
