e7597525da970b22e4611f41f7f01aed22ac97f2
Mohammad Shabaj Khan <42003336+ShahbajKhan@users.noreply.github.com>
2022-10-02 13:55:19 +0600

    this commit contains the content of video seven

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -15,7 +15,7 @@ const Orders = () => {
 
     return (
         <div className='shop-container'>
-            <div className="products-container">
+            <div className="review-items-container">
                 {
                     cart.map(product => <ReviewItem
                         key={product.id}
